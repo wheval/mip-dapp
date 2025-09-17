9521ef99f89c9646c1e8b92cdd0612a63f7dd3c7
salvador camino <salvadorcamino@gmail.com>
2025-09-17 16:59:37 -0300

    route settings

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,13 +1,14 @@
 import { clerkMiddleware, createRouteMatcher, clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/onboarding"])
+//const isPublicRoute = createRouteMatcher(["/", "/onboarding"])
+const isProtectedRoute = createRouteMatcher(['/portfolio', '/create'])
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
 
   // If the user isn't signed in and the route is private, redirect to sign-in
-  if (!userId && !isPublicRoute(req)) {
+  if (!userId && !isProtectedRoute(req)) {
     return redirectToSignIn({ returnBackUrl: req.url });
   }
 
@@ -39,7 +40,7 @@ export default clerkMiddleware(async (auth, req) => {
   }
 
   // If the user is logged in and the route is protected, let them view.
-  if (userId && !isPublicRoute(req)) {
+  if (userId && !isProtectedRoute(req)) {
     return NextResponse.next();
   }
 
